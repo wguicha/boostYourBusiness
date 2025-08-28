18d4392a7a0c54b7c58b05f11bab31bd060ad62e
wguicha <williamalberto1@hotmail.com>
2025-08-28 15:42:25 -0500

    auth adjust

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,6 @@
 import type { NextAuthConfig } from 'next-auth';
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
